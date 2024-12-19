b6da610a740dd8ae23c27335758d6d355a5fb675
Najib Fikri <najibfikri13@gmail.com>
2024-12-19 15:27:14 +0700

    feat: add services for accounts, cards, and loans
    - Added `accounts` service with controller, service, repository, DTOs, entities, exception handling, and audit support.
    - Added `cards` service with controller, service, repository, DTOs, entities, exception handling, and audit support.
    - Added `loans` service with controller, service, repository, DTOs, entities, exception handling, and audit support.
    - Configured project structure and added `.idea` files for IDE support.
    - Included Maven wrapper files for all microservices.
    - Added schema files and application configurations for each service.

---
+++ section2/accounts/src/main/java/com/najibfikri/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.najibfikri.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.najibfikri.accounts.controller") })
+@EnableJpaRepositories("com.najibfikri.accounts.repository")
+@EntityScan("com.najibfikri.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@najibfikri.com",
+						url = "https://www.najibfikri.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.najibfikri.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.najibfikri.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
