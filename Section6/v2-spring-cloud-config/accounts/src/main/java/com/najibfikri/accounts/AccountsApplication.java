a0300ef82dae3d137fd843c9cd8520c63783eb29
Najib Fikri <najibfikri13@gmail.com>
2024-12-20 20:02:13 +0700

    feat: copy all implementations from `Section6/v1-springboot` to `Section6/v2-spring-cloud-config`

---
+++ Section6/v2-spring-cloud-config/accounts/src/main/java/com/najibfikri/accounts/AccountsApplication.java
@@ -0,0 +1,46 @@
+package com.najibfikri.accounts;
+
+import com.najibfikri.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.najibfikri.accounts.controller") })
+@EnableJpaRepositories("com.najibfikri.accounts.repository")
+@EntityScan("com.najibfikri.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Madan Reddy",
+						email = "tutor@najibfikri.com",
+						url = "https://www.najibfikri.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.najibfikri.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.najibfikri.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
