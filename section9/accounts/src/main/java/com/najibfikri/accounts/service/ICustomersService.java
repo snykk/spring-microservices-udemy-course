5b9ee5d1b16aa214f9a8438cf7756767232cb82b
Najib Fikri <najibfikri13@gmail.com>
2025-01-02 16:24:17 +0700

    feat: copy all implementations from  to

---
+++ section9/accounts/src/main/java/com/najibfikri/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.najibfikri.accounts.service;
+
+import com.najibfikri.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
