86b53689d8e9998d7570318978a2c9e462b1032a
Najib Fikri <najibfikri13@gmail.com>
2025-02-12 11:14:13 +0700

    feat: copy all implementations of accounts, cards, configserver, gatewayserver, loans, and messages from `section_14` to `section_17`

---
+++ section_17/accounts/src/main/java/com/najibfikri/accounts/service/client/CardsFallback.java
@@ -0,0 +1,15 @@
+package com.najibfikri.accounts.service.client;
+
+import com.najibfikri.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+}
